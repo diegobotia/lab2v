94f9f80cf705a4b6d0e61ef31934191fab284c8f
DB <diego.botia@udea.edu.co>
2025-10-14 17:05:50 -0500

    Repo base

---
+++ src/main/java/com/udea/lab2v/Lab2vApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2vApplication.class, args);
+	}
+
+}
